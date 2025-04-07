4475840fb6e25f914a941ed720d46c7f51ae96b6
Suresh Polai <sureshdulupolai@gmail.com>
2025-04-07 22:18:37 +0530

    update 1

--- ID_Generator.py
+++ ID_Generator.py
@@ -1,9 +1,35 @@
+import random
+
 lstOfCompanyId = [
     'ADH', 'DR', 'BVI',
 ]
 
-def idGeneratorForCompany(CompanyCode):
-    print(CompanyCode)
+def idGeneratorForCompany(CompanyCode, Loop = 1):
+    NewCompanyCodeLstOfUser = []
+    for Lp in range(Loop):
+        CC = CompanyCode
+        RC = str(random.randint(1, 10))
+        CCRC = CC + RC
+        if len(NewCompanyCodeLstOfUser) > 0:
+            for NccuData in NewCompanyCodeLstOfUser:
+                NoOfCode = ''
+                for a1 in NccuData:
+                    if a1.isdigit():
+                        NoOfCode += str(a1)
+            print(NoOfCode)
+
+            # for NCCU in NewCompanyCodeLstOfUser:
+            #     if CCRC not in NCCU:
+            #         NewCompanyCodeLstOfUser += [CCRC]
+            #         break
+            #     else:
+            #         continue
+        else:
+            NewCompanyCodeLstOfUser += [CCRC]
+    
+    # print(len(NewCompanyCodeLstOfUser))
+    # print()
+    # print(NewCompanyCodeLstOfUser)
 
 CodeOfCompanyPassing = lstOfCompanyId[0]
-idGeneratorForCompany(CompanyCode = CodeOfCompanyPassing)
+idGeneratorForCompany(CompanyCode = CodeOfCompanyPassing, Loop = 100)
