7ef16958a3f419cfc189cc055c1a8b1d8782e9ff
Suresh Polai <sureshdulupolai@gmail.com>
2025-04-07 21:59:00 +0530

    Create a Id Generator For My Django Project in Python

---
+++ ID_Generator.py
@@ -0,0 +1,9 @@
+lstOfCompanyId = [
+    'ADH', 'DR', 'BVI',
+]
+
+def idGeneratorForCompany(CompanyCode):
+    print(CompanyCode)
+
+CodeOfCompanyPassing = lstOfCompanyId[0]
+idGeneratorForCompany(CompanyCode = CodeOfCompanyPassing)
