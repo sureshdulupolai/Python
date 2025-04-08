3f32c884297c2a4fcbc26aad8a8571fa830c1142
Suresh Polai <sureshdulupolai@gmail.com>
2025-04-08 11:25:31 +0530

    Successfull IdGenerator Function Created

--- ID_Generator.py
+++ ID_Generator.py
@@ -8,26 +8,33 @@ def idGeneratorForCompany(CompanyCode, Loop = 1):
     NewCompanyCodeLstOfUser = []
     for Lp in range(Loop):
         CC = CompanyCode
-        RC = str(random.randint(12, 30))
+        RC = str(random.randint(1, 2))
         CCRC = CC + RC
         if len(NewCompanyCodeLstOfUser) > 0:
+            CopyDataPresent = 0
+            # for loop using to check all id that created in list, where it is same or not
             for NccuData in NewCompanyCodeLstOfUser:
                 NoOfCode = ''
+                # for loop in a particular id that already genrated inside list
                 for a1 in NccuData:
+                    # check in id generator there is a number then condition will True else False
                     if a1.isdigit():
                         NoOfCode += str(a1)
-                
-                if int(RC) != int(NoOfCode):
-                    NewCompanyCodeLstOfUser += [CCRC]
-                    break   
-                else:
-                    continue
+                        # check if RC Present in list or not
+                        if int(RC) == int(NoOfCode):
+                            CopyDataPresent += 1   
+                        else:
+                            continue
+            if CopyDataPresent == 0:
+                NewCompanyCodeLstOfUser += [CCRC]
+            else:
+                pass
         else:
             NewCompanyCodeLstOfUser += [CCRC]
 
-    print(len(NewCompanyCodeLstOfUser))
+    # print(len(NewCompanyCodeLstOfUser))
     # print()
-    print(NewCompanyCodeLstOfUser)
+    # print(NewCompanyCodeLstOfUser)
 
 CodeOfCompanyPassing = lstOfCompanyId[0]
 idGeneratorForCompany(CompanyCode = CodeOfCompanyPassing, Loop = 5)
