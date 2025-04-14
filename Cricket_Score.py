84a053cc7151166c650b877ad5ad83059af48cf6
Suresh Polai <sureshdulupolai@gmail.com>
2025-04-14 22:15:00 +0530

    finding current run rate in pr over 1

--- Cricket_Score.py
+++ Cricket_Score.py
@@ -1,9 +1,11 @@
 # 6 over = 4.5 Run Rate
 # 153 Target, Abhi Uska Run Rate Kitne Hoga
 
+import math
+
 TotalRun = 153
 OverDone = 6
-RunGet = (4.5 + (6 * OverDone))
+RunGet = (4.5 * OverDone)
 
 RunLeft = float(TotalRun) - float(RunGet)
 TotalBallLeft = 20 - OverDone
@@ -12,5 +14,5 @@ CurrentRunRate = round(RunLeft / TotalBallLeft, 2)
 
 print('Current Run Rate : {}'.format(CurrentRunRate))
 
-prBallRun = round((CurrentRunRate / 6), 2)
+prBallRun = math.ceil(round((CurrentRunRate / 6), 2))
 print('Pr Ball Run Rate : {}'.format(prBallRun))
